df847ce70fd6b8ca70ba2d5e1bd3b51e520ab8b4
qiushaowei <qiushaowei@360.net>
2019-01-14 10:46:12 +0800

    up

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-# chinaal
-
-China Administration Layout - 中国行政规划 JS 数据
-
-[http://www.mca.gov.cn/article/sj/xzqh](http://www.mca.gov.cn/article/sj/xzqh)
+# babelexample
+example for sample babel js project
---
+++ bin/app.js
@@ -0,0 +1,211 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+exports.init = init;
+
+var _fs = require("fs");
+
+var _fs2 = _interopRequireDefault(_fs);
+
+var _path = require("path");
+
+var _path2 = _interopRequireDefault(_path);
+
+var _chalk = require("chalk");
+
+var _chalk2 = _interopRequireDefault(_chalk);
+
+var _clear = require("clear");
+
+var _clear2 = _interopRequireDefault(_clear);
+
+var _figlet = require("figlet");
+
+var _figlet2 = _interopRequireDefault(_figlet);
+
+var _shelljs = require("shelljs");
+
+var _shelljs2 = _interopRequireDefault(_shelljs);
+
+var _inquirer = require("inquirer");
+
+var _inquirer2 = _interopRequireDefault(_inquirer);
+
+var _constant = require("./data/constant.js");
+
+var CONST = _interopRequireWildcard(_constant);
+
+var _data = require("./data/data.js");
+
+var DATA = _interopRequireWildcard(_data);
+
+var _ProjectExample = require("./ProjectExample.js");
+
+var _ProjectExample2 = _interopRequireDefault(_ProjectExample);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var error = _chalk2.default.bold.red;
+var warning = _chalk2.default.keyword('orange');
+var success = _chalk2.default.greenBright;
+var info = _chalk2.default.bold.blue;
+
+var App = function () {
+    function App(appRoot, projectRoot, packJSON) {
+        _classCallCheck(this, App);
+
+        this.appRoot = appRoot;
+        this.projectRoot = projectRoot;
+        this.packJSON = packJSON;
+
+        console.log(['appRoot: ' + this.appRoot, 'projectRoot: ' + this.projectRoot].join("\n"));
+
+        this.init();
+
+        console.log();
+    }
+
+    _createClass(App, [{
+        key: "init",
+        value: function init() {
+            var _this = this;
+
+            (0, _clear2.default)();
+
+            this.project;
+            this.prompt = _inquirer2.default.createPromptModule();
+            this.welcome();
+
+            this.system = 1;
+
+            /*
+            shell.exec(  [ 
+                'cd ' + this.projectRoot
+                , 'git config core.fileMode false && git config core.autocrlf false'
+            ].join('&&') );
+            */
+
+            console.log();
+            new Promise(function (resolve) {
+                setTimeout(resolve, 1);
+            }).then(function () {
+                return _this.getExample();
+            }).then(function () {
+                _this.isGood = 1;
+
+                return new Promise(function (resolve) {
+                    setTimeout(resolve, 1);
+                });
+            }).then(function () {
+                if (!_this.isGood) {
+                    _this.confirm = 'no';
+                    return;
+                }
+                return _this.getConfirm();
+            }).then(function () {
+                if (_this.confirm == 'no') return;
+                _this.project = new _ProjectExample2.default(_this);
+            });
+        }
+    }, {
+        key: "getConfirm",
+        value: function () {
+            var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+                var data;
+                return regeneratorRuntime.wrap(function _callee$(_context) {
+                    while (1) {
+                        switch (_context.prev = _context.next) {
+                            case 0:
+                                _context.next = 2;
+                                return this.prompt(DATA.Q_CONFIRM);
+
+                            case 2:
+                                data = _context.sent;
+
+                                this.confirm = data.confirm;
+
+                            case 4:
+                            case "end":
+                                return _context.stop();
+                        }
+                    }
+                }, _callee, this);
+            }));
+
+            function getConfirm() {
+                return _ref.apply(this, arguments);
+            }
+
+            return getConfirm;
+        }()
+    }, {
+        key: "getExample",
+        value: function () {
+            var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2() {
+                var data;
+                return regeneratorRuntime.wrap(function _callee2$(_context2) {
+                    while (1) {
+                        switch (_context2.prev = _context2.next) {
+                            case 0:
+                                _context2.next = 2;
+                                return this.prompt(DATA.Q_EXAMPLE);
+
+                            case 2:
+                                data = _context2.sent;
+
+                                this.example = data.example;
+
+                            case 4:
+                            case "end":
+                                return _context2.stop();
+                        }
+                    }
+                }, _callee2, this);
+            }));
+
+            function getExample() {
+                return _ref2.apply(this, arguments);
+            }
+
+            return getExample;
+        }()
+    }, {
+        key: "fileExists",
+        value: function fileExists(file) {
+            return _fs2.default.existsSync(file);
+        }
+    }, {
+        key: "welcome",
+        value: function welcome() {
+            console.log(_chalk2.default.yellow(_figlet2.default.textSync(CONST.APPNAME, { horizontalLayout: 'full' })));
+            console.log(_chalk2.default.bold.yellow(CONST.TITLE + " - " + this.packJSON.version));
+            console.log();
+            console.log(info("github: " + this.packJSON.repository.url));
+
+            console.log();
+            console.log(info('使用:'));
+            console.log(info('     方法1: 使用说明'));
+            console.log();
+            console.log(info('     方法2: 使用说明'));
+            console.log();
+        }
+    }]);
+
+    return App;
+}();
+
+exports.default = App;
+function init(APP_ROOT, PROJECT_ROOT, packJSON) {
+    var AppIns = new App(APP_ROOT, PROJECT_ROOT, packJSON);
+}
