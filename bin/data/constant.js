abb02e40537d260b368ed6295fe654d7cef44f5b
qiushaowei <qiushaowei@360.net>
2019-01-14 11:12:40 +0800

    up

--- bin/data/constant.js
+++ bin/data/constant.js
@@ -3,8 +3,8 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var APPNAME = exports.APPNAME = 'BABEL_EXAMPLE';
-var TITLE = exports.TITLE = 'BABEL EXAMPLE';
+var APPNAME = exports.APPNAME = 'chinaal';
+var TITLE = exports.TITLE = 'China Administration Layout';
 
 var PROJECT_ROOT_NOT_EXISTS = exports.PROJECT_ROOT_NOT_EXISTS = "找不到项目根目录, 请指定有效的目录";
 
--- src/data/constant.js
+++ src/data/constant.js
@@ -1,6 +1,6 @@
 
 
-export const APPNAME = 'BABEL_EXAMPLE';
+export const APPNAME = 'chinaal';
 export const TITLE = 'China Administration Layout';
 
 export const PROJECT_ROOT_NOT_EXISTS = "找不到项目根目录, 请指定有效的目录";
